131543818be9fd7468a0c883c19d94f33f4f7047
davemlz <dml.mont@gmail.com>
2021-07-25 14:59:43 +0200

    Spectral module added

---
+++ ee_extra/spectral/core.py
@@ -0,0 +1,184 @@
+import ee
+import json
+import pkg_resources
+import os
+import warnings
+import requests
+import re
+from typing import Optional, Union
+from ee_extra.spectral.utils import _get_expression_map, _get_indices, _get_kernel_image, _remove_none_dict, _get_kernel_parameters
+from ee_extra.STAC import _get_platform_STAC
+
+def spectralIndices(
+    x: Union[ee.Image, ee.ImageCollection],
+    index: Optonal[Union[str, list[str]]] = "NDVI",
+    G: Optional[Union[float, int]] = 2.5,
+    C1: Optional[Union[float, int]] = 6.0,
+    C2: Opional[Union[float, int]] = 7.5,
+    L: Optional[Union[float, int]] = 1.0,
+    cexp: Optional[Union[float, int]] = 1.16,
+    nexp: Optional[Union[float, int]] = 2.0,
+    alpha: Optional[Union[float, int]] = 0.1,
+    slope: Optional[Union[float, int]] = 1.0,
+    intercept: Optional[Union[float, int]] = 0.0,
+    kernel: Optional[str] = "RBF",
+    sigma: Optional[Union[float, str]] = "0.5 * (a + b)",
+    p: Optional[Union[float, int]] = 2,
+    c: Optional[Union[float, int]] = 1.0,
+    online: Optional[bool] = False,
+) -> Union[ee.Image, ee.ImageCollection]:
+    """Computes one or more spectral indices (indices are added as bands) for an image oir image collection.
+
+    Args:    
+        x : Image or Image Collectionto compute indices on. Must be scaled to [0,1].        
+        index : Index or list of indices to compute.
+        G : Gain factor. Used just for index = 'EVI'.
+        C1 : Coefficient 1 for the aerosol resistance term. Used just for index = 'EVI'.
+        C2 : Coefficient 2 for the aerosol resistance term. Used just for index = 'EVI'.
+        L : Canopy background adjustment. Used just for index = ['EVI','SAVI'].
+        cexp : Exponent used for OCVI.
+        nexp : Exponent used for GDVI.
+        alpha : Weighting coefficient  used for WDRVI.
+        slope : Soil line slope.
+        intercept : Soil line intercept.
+        kernel : Kernel used for kernel indices. One of 'linear', 'RBF', 'poly'.
+        sigma : Length-scale parameter. Used for kernel = 'RBF'. If str, this must be an expression including 'a' and 'b'. If numeric, this must be positive.
+        p : Kernel degree. Used for kernel = 'poly'.
+        c : Free parameter that trades off the influence of higher-order versus lower-order terms. Used for kernel = 'poly'. This must be greater than or equal to 0.
+        online : Wheter to retrieve the most recent list of indices directly from the GitHub repository and not from the local copy.
+
+    Returns:
+        Image or Image Collection with the computed spectral index, or indices, as new bands.
+
+    Examples:
+        >>> import ee
+        >>> from ee_extra.spectral.core import spectralIndices
+        >>> ee.Initialize()
+        >>> S2 = ee.ImageCollection("COPERNICUS/S2_SR")
+        >>> spectralIndices(S2,["NDVI","SAVI"],L = 0.5)
+    """
+    platformDict = _get_platform_STAC(x)
+
+    if isinstance(sigma, int) or isinstance(sigma, float):
+        if sigma < 0:
+            raise Exception("[sigma] must be positive!")
+
+    if p <= 0 or c < 0:
+        raise Exception("[p] and [c] must be positive!")
+
+    additionalParameters = {
+        "g": float(G),
+        "C1": float(C1),
+        "C2": float(C2),
+        "L": float(L),
+        "cexp": float(cexp),
+        "nexp": float(nexp),
+        "alpha": float(alpha),
+        "sla": float(slope),
+        "slb": float(intercept),
+        "p": float(p),
+        "c": float(c),
+    }
+
+    spectralIndices = _get_indices(online)
+    indicesNames = list(spectralIndices.keys())
+
+    if not isinstance(index, list):
+        if index == "all":
+            index = list(spectralIndices.keys())
+        elif index in [
+            "vegetation",
+            "burn",
+            "water",
+            "snow",
+            "drought",
+            "urban",
+            "kernel",
+        ]:
+            temporalListOfIndices = []
+            for idx in indicesNames:
+                if spectralIndices[idx]["type"] == index:
+                    temporalListOfIndices.append(idx)
+            index = temporalListOfIndices
+        else:
+            index = [index]
+
+    for idx in index:
+        if idx not in list(spectralIndices.keys()):
+            warnings.warn(
+                "Index " + idx + " is not a built-in index and it won't be computed!"
+            )
+        else:
+
+            def temporalIndex(img):
+                lookupDic = _get_expression_map(img, platformDict)
+                lookupDic = {**lookupDic, **additionalParameters}
+                kernelParameters = _get_kernel_parameters(img, lookupDic, kernel, sigma)
+                lookupDic = {**lookupDic, **kernelParameters}
+                lookupDicCurated = _remove_none_dict(lookupDic)
+                if all(
+                    band in list(lookupDicCurated.keys())
+                    for band in spectralIndices[idx]["bands"]
+                ):
+                    return img.addBands(
+                        img.expression(
+                            spectralIndices[idx]["formula"], lookupDicCurated
+                        ).rename(idx)
+                    )
+                else:
+                    warnings.warn(
+                        "This platform doesn't have the required bands for "
+                        + idx
+                        + " computation!"
+                    )
+                    return img
+
+            if isinstance(x, ee.imagecollection.ImageCollection):
+                x = x.map(temporalIndex)
+            elif isinstance(x, ee.image.Image):
+                x = temporalIndex(x)
+
+    return x
+
+
+def indices(online: Optional[bool] = False) -> dict:
+    """Gets the dictionary of available indices.
+
+    Args:    
+        online : Whether to retrieve the most recent list of indices directly from the GitHub repository and not from the local copy.
+
+    Returns:
+        Dictionary of available indices.
+    
+    Examples:
+        >>> import ee
+        >>> from ee_extra.spectral.core import indices
+        >>> ee.Initialize()
+        >>> ind = indices()
+        >>> ind.BAIS2.long_name
+        'Burned Area Index for Sentinel 2'
+        >>> ind.BAIS2.formula
+        '(1.0 - ((RE2 * RE3 * RE4) / R) ** 0.5) * (((S2 - RE4)/(S2 + RE4) ** 0.5) + 1.0)'
+        >>> ind.BAIS2.reference
+        'https://doi.org/10.3390/ecrs-2-05177'
+    """
+    return _get_indices(online)
+
+
+def listIndices(online: Optional[bool] = False) -> list:
+    """Gets the list of available indices.
+
+    Args:    
+        online : Whether to retrieve the most recent list of indices directly from the GitHub repository and not from the local copy.
+
+    Returns:
+        List of available indices.
+
+    Examples:
+        >>> import ee
+        >>> from ee_extra.spectral.core import listIndices
+        >>> ee.Initialize()
+        >>> listIndices()
+        ['BNDVI','CIG','CVI','EVI','EVI2','GBNDVI','GNDVI',...]
+    """
+    return list(_get_indices(online).keys())
---
+++ ee_extra/spectral/utils.py
@@ -0,0 +1,267 @@
+import ee
+import json
+import pkg_resources
+import os
+import warnings
+import requests
+import re
+from typing import Optional, Union
+from ee_extra.STAC import _get_platform_STAC
+
+def _get_expression_map(img, platformDict):
+    """Gets the dictionary required for the map parameter in ee.Image.expression() method.
+
+    Parameters
+    ----------
+    img : ee.Image
+        Image to get the dictionary from.
+    platformDict : dict
+        Dictionary retrieved from the _get_platform() method.
+
+    Returns
+    -------
+    dict
+        Map dictionary for ee.Image.expression.
+    """
+
+    def lookupS2(img):
+        return {
+            "A": img.select("B1"),
+            "B": img.select("B2"),
+            "G": img.select("B3"),
+            "R": img.select("B4"),
+            "RE1": img.select("B5"),
+            "RE2": img.select("B6"),
+            "RE3": img.select("B7"),
+            "N": img.select("B8"),
+            "RE4": img.select("B8A"),
+            "WV": img.select("B9"),
+            "S1": img.select("B11"),
+            "S2": img.select("B12"),
+        }
+
+    def lookupL8(img):
+        return {
+            "A": img.select("B1"),
+            "B": img.select("B2"),
+            "G": img.select("B3"),
+            "R": img.select("B4"),
+            "N": img.select("B5"),
+            "S1": img.select("B6"),
+            "S2": img.select("B7"),
+            "T1": img.select("B10"),
+            "T2": img.select("B11"),
+        }
+
+    def lookupL8C2(img):
+        return {
+            "A": img.select("SR_B1"),
+            "B": img.select("SR_B2"),
+            "G": img.select("SR_B3"),
+            "R": img.select("SR_B4"),
+            "N": img.select("SR_B5"),
+            "S1": img.select("SR_B6"),
+            "S2": img.select("SR_B7"),
+            "T1": img.select("ST_B10"),
+        }
+
+    def lookupL457(img):
+        return {
+            "B": img.select("B1"),
+            "G": img.select("B2"),
+            "R": img.select("B3"),
+            "N": img.select("B4"),
+            "S1": img.select("B5"),
+            "T1": img.select("B6"),
+            "S2": img.select("B7"),
+        }
+
+    def lookupL7C2(img):
+        return {
+            "B": img.select("SR_B1"),
+            "G": img.select("SR_B2"),
+            "R": img.select("SR_B3"),
+            "N": img.select("SR_B4"),
+            "S1": img.select("SR_B5"),
+            "T1": img.select("ST_B6"),
+            "S2": img.select("SR_B7"),
+        }
+
+    def lookupMOD09GQ(img):
+        return {"R": img.select("sur_refl_b01"), "N": img.select("sur_refl_b02")}
+
+    def lookupMOD09GA(img):
+        return {
+            "B": img.select("sur_refl_b03"),
+            "G": img.select("sur_refl_b04"),
+            "R": img.select("sur_refl_b01"),
+            "N": img.select("sur_refl_b02"),
+            "S1": img.select("sur_refl_b06"),
+            "S2": img.select("sur_refl_b07"),
+        }
+
+    def lookupMCD43A4(img):
+        return {
+            "B": img.select("Nadir_Reflectance_Band3"),
+            "G": img.select("Nadir_Reflectance_Band4"),
+            "R": img.select("Nadir_Reflectance_Band1"),
+            "N": img.select("Nadir_Reflectance_Band2"),
+            "S1": img.select("Nadir_Reflectance_Band6"),
+            "S2": img.select("Nadir_Reflectance_Band7"),
+        }
+
+    lookupPlatform = {
+        "COPERNICUS/S2": lookupS2,
+        "COPERNICUS/S2_SR": lookupS2,
+        "LANDSAT/LC08/C01/T1_SR": lookupL8,
+        "LANDSAT/LC08/C01/T2_SR": lookupL8,
+        "LANDSAT/LC08/C02/T1_L2": lookupL8C2,
+        "LANDSAT/LE07/C01/T1_SR": lookupL457,
+        "LANDSAT/LE07/C01/T2_SR": lookupL457,
+        "LANDSAT/LE07/C02/T1_L2": lookupL7C2,
+        "LANDSAT/LT05/C01/T1_SR": lookupL457,
+        "LANDSAT/LT05/C01/T2_SR": lookupL457,
+        "LANDSAT/LT04/C01/T1_SR": lookupL457,
+        "LANDSAT/LT04/C01/T2_SR": lookupL457,
+        "MODIS/006/MOD09GQ": lookupMOD09GQ,
+        "MODIS/006/MYD09GQ": lookupMOD09GQ,
+        "MODIS/006/MOD09GA": lookupMOD09GA,
+        "MODIS/006/MYD09GA": lookupMOD09GA,
+        "MODIS/006/MOD09Q1": lookupMOD09GQ,
+        "MODIS/006/MYD09Q1": lookupMOD09GQ,
+        "MODIS/006/MOD09A1": lookupMOD09GA,
+        "MODIS/006/MYD09A1": lookupMOD09GA,
+        "MODIS/006/MCD43A4": lookupMCD43A4,
+    }
+
+    if platformDict["platform"] not in list(lookupPlatform.keys()):
+        raise Exception(
+            "Sorry, satellite platform not supported for index computation!"
+        )
+
+    return lookupPlatform[platformDict["platform"]](img)
+
+
+def _get_indices(online):
+    """Retrieves the dictionary of indices used for the index() method in ee.Image and ee.ImageCollection classes.
+
+    Parameters
+    ----------
+    online : boolean
+        Wheter to retrieve the most recent list of indices directly from the GitHub repository and not from the local copy.
+
+    Returns
+    -------
+    dict
+        Indices.
+    """
+    if online:
+        indices = requests.get(
+            "https://raw.githubusercontent.com/davemlz/awesome-ee-spectral-indices/main/output/spectral-indices-dict.json"
+        ).json()
+    else:
+        eeExtraDir = os.path.dirname(
+            pkg_resources.resource_filename("ee_extra", "ee_extra.py")
+        )
+        dataPath = os.path.join(eeExtraDir, "data/spectral-indices-dict.json")
+        f = open(dataPath)
+        indices = json.load(f)
+
+    return indices["SpectralIndices"]
+
+
+def _get_kernel_image(img, lookup, kernel, sigma, a, b):
+    """Creates an ee.Image representing a kernel computed on bands [a] and [b].
+
+    Parameters
+    ----------
+    img : ee.Image
+        Image to compute the kernel on.
+    lookup : dict
+        Dictionary retrieved from _get_expression_map().
+    kernel : str
+        Kernel to use.
+    sigma : str | float
+        Length-scale parameter. Used for kernel = 'RBF'.
+    a : str
+        Key of the first band to use.
+    b : str
+        Key of the second band to use.
+
+    Returns
+    -------
+    ee.Image
+        Kernel image.
+    """
+    if a not in list(lookup.keys()) or b not in list(lookup.keys()):
+        return None
+    else:
+        lookupab = {"a": lookup[a], "b": lookup[b]}
+        if isinstance(sigma, str):
+            lookup = {**lookup, **lookupab, "sigma": img.expression(sigma, lookupab)}
+        else:
+            lookup = {**lookup, **lookupab, "sigma": sigma}
+        kernels = {
+            "linear": "a * b",
+            "RBF": "exp((-1.0 * (a - b) ** 2.0)/(2.0 * sigma ** 2.0))",
+            "poly": "((a * b) + c) ** p",
+        }
+        return img.expression(kernels[kernel], lookup)
+
+
+def _remove_none_dict(dictionary):
+    """Removes elements from a dictionary with None values.
+
+    Parameters
+    ----------
+    dictionary : dict
+
+    Returns
+    -------
+    dict
+        Curated dictionary.
+    """
+    newDictionary = dict(dictionary)
+    for key in dictionary.keys():
+        if dictionary[key] is None:
+            del newDictionary[key]
+    return newDictionary
+
+
+def _get_kernel_parameters(img, lookup, kernel, sigma):
+    """Gets the additional kernel parameters to compute kernel indices.
+
+    Parameters
+    ----------
+    img : ee.Image
+        Image to compute the kernel parameters on.
+    lookup : dict
+        Dictionary retrieved from _get_expression_map().
+    kernel : str
+        Kernel to use.
+    sigma : str | float
+        Length-scale parameter. Used for kernel = 'RBF'.
+
+    Returns
+    -------
+    dict
+        Kernel parameters.
+    """
+    kernelParameters = {
+        "kNN": _get_kernel_image(img, lookup, kernel, sigma, "N", "N"),
+        "kNR": _get_kernel_image(img, lookup, kernel, sigma, "N", "R"),
+        "kNB": _get_kernel_image(img, lookup, kernel, sigma, "N", "B"),
+        "kNL": _get_kernel_image(img, lookup, kernel, sigma, "N", "L"),
+        "kGG": _get_kernel_image(img, lookup, kernel, sigma, "G", "G"),
+        "kGR": _get_kernel_image(img, lookup, kernel, sigma, "G", "R"),
+        "kGB": _get_kernel_image(img, lookup, kernel, sigma, "G", "B"),
+        "kBB": _get_kernel_image(img, lookup, kernel, sigma, "B", "B"),
+        "kBR": _get_kernel_image(img, lookup, kernel, sigma, "B", "R"),
+        "kBL": _get_kernel_image(img, lookup, kernel, sigma, "B", "L"),
+        "kRR": _get_kernel_image(img, lookup, kernel, sigma, "R", "R"),
+        "kRB": _get_kernel_image(img, lookup, kernel, sigma, "R", "B"),
+        "kRL": _get_kernel_image(img, lookup, kernel, sigma, "R", "L"),
+        "kLL": _get_kernel_image(img, lookup, kernel, sigma, "L", "L"),
+    }
+
+    return kernelParameters

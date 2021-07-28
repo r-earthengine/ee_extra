33bb9e0dcd04670456f9e5779a83cd7584bbda1d
csaybar <csaybar@gmail.com>
2021-07-28 13:57:33 +0200

    doc up

--- ee_extra/JavaScript/eejs2py.py
+++ ee_extra/JavaScript/eejs2py.py
@@ -1,10 +1,19 @@
-"""JavaScript module provide functions to evaluate JS 
-code in the EE session. The following functions are
-implemented:
+"""JavaScript module provide functions to transform 
+Eath Engine (EE) JavaScript code to Python code. The following
+functions are implemented:
 
-I. Evaluate JS code
+I. Functions
 
-- evaluate: Evaluate a JS string code.
+* _convert_path_to_ee_sources: Get the remote module path from the 'ee-sources' GCS bucket.
+* _get_ee_sources_path: Get the local module path.
+* _convert_path_to_ee_extra: Convert remote module path to local module path.
+* _check_if_module_exists: Check if a EE local module exists.
+* _open_module_as_str: Open a EE local module as a string.
+* _get_dependencies: Get the dependencies of an EE complex module.
+* _install: Install an specific EE Js file in their system.
+* install: Install an specific EE Js module in their system.
+* uninstall: Install an specific EE Js module.
+* translate: Translate a EE Js module to a Python script.
 """
 
 import json
@@ -19,8 +28,8 @@ from js2py import EvalJs
 
 
 def _convert_path_to_ee_sources(path: str) -> str:
-    """Convert an Earth Engine module path into an ee-sources module url.
-
+    """Get the remote module path from the 'ee-sources' GCS bucket.
+    
     Args:
         path: str
 
--- ee_extra/ee-sources/users/aazuspan/geeSharp/spectral.js
+++
@@ -1,1 +0,0 @@
-<?xml version='1.0' encoding='UTF-8'?><Error><Code>NoSuchKey</Code><Message>The specified key does not exist.</Message><Details>No such object: ee-sources/users/aazuspan/geeSharp/spectral.js</Details></Error>

48fb5d9a11fd3bf9d9e4ba9e10b11048bd1a2fbe
Aaron Zuspan <aa.zuspan@gmail.com>
2022-01-02 14:54:43 -0800

    Add case-insensitive matching function

--- ee_extra/utils.py
+++ ee_extra/utils.py
@@ -1,6 +1,7 @@
+import difflib
 import json
 import os
-from typing import Any, Optional, Union
+from typing import Any, Optional, Union, List
 
 import pkg_resources
 
@@ -22,3 +23,29 @@ def _load_JSON(x: Optional[str] = "ee-catalog-ids.json") -> Any:
     data = json.load(f)
 
     return data
+
+
+def _get_case_insensitive_close_matches(
+    word: str, possibilities: List[str], n: int = 3, cutoff: float = 0.6
+) -> List[str]:
+    """A case-insensitive wrapper around difflib.get_close_matches.
+
+    Args:
+        word : A string for which close matches are desired.
+        possibilites : A list of strings against which to match word.
+        n : the maximum number of close matches to return. n must be > 0.
+        cutoff : Possibilities that don't score at least that similar to word are ignored.
+
+    Returns:
+        The best (no more than n) matches among the possibilities are returned in a list,
+        sorted by similarity score, most similar first.
+
+    Examples:
+        >>> from ee_extra.utils import _get_case_insensitive_close_matches
+        >>> _get_case_insensitive_close_matches("mse", ["MSE", "ERGAS"])
+        ["MSE"]
+    """
+    lower_matches = difflib.get_close_matches(
+        word.lower(), [p.lower() for p in possibilities], n, cutoff
+    )
+    return [p for p in possibilities if p.lower() in lower_matches]

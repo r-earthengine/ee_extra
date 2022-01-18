137722fa07d0c57263177dd32e6cd900bd3afffe
Aaron Zuspan <aa.zuspan@gmail.com>
2022-01-17 17:28:12 -0800

    Test metrics module (#31) and make tests a module

---
+++ tests/test_QA.py
@@ -0,0 +1,36 @@
+import ee
+import unittest
+
+from ee_extra.QA.metrics import *
+
+ee.Initialize()
+
+
+class Test(unittest.TestCase):
+    """Tests for QA module."""
+
+    def test_list_metrics(self):
+        """Test that listMetrics returns a valid dictionary"""
+        metrics = listMetrics()
+
+        self.assertIn("MSE", metrics.keys())
+        self.assertEqual(MSE, metrics["MSE"])
+
+    def test_get_metrics(self):
+        """Test that getMetrics returns matches"""
+        mse = getMetrics(["MSE"])[0]
+        self.assertIs(mse, MSE)
+
+    def test_all_metrics(self):
+        """Test that all metrics run"""
+        metrics = listMetrics().values()
+
+        img = ee.Image("LANDSAT/LC08/C01/T1_TOA/LC08_047027_20160819")
+        modified = ee.Image("LANDSAT/LC08/C01/T1_TOA/LC08_047027_20160819")
+
+        for metric in metrics:
+            if metric is ERGAS:
+                value = metric(img, modified, h=30, l=30)
+            else:
+                value = metric(img, modified)
+                self.assertIsInstance(value, (ee.Number, ee.Dictionary))

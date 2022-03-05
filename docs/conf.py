e46165e2427845f830a0732b4953340892294886
davemlz <dml.mont@gmail.com>
2022-03-05 21:35:00 +0100

    VERSION: 0.0.11

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Cesar Aybar and David Montero Loaiza"
 author = "Cesar Aybar and David Montero Loaiza"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.10"
+release = "0.0.11"
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(filename):
 
 setup(
     name="ee_extra",
-    version="0.0.10",
+    version="0.0.11",
     url="https://github.com/r-earthengine/ee_extra",
     license="Apache 2.0",
     author="Cesar Aybar, David Montero Loaiza and Aaron Zuspan",

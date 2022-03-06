e3c832f2196f7a5d6b0c30311c9e841946ed8182
David Montero Loaiza <49817852+davemlz@users.noreply.github.com>
2022-03-06 13:46:09 +0100

    VERSION: 0.0.12

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Cesar Aybar and David Montero Loaiza"
 author = "Cesar Aybar and David Montero Loaiza"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.11"
+release = "0.0.12"
 
 # -- General configuration ---------------------------------------------------
 
